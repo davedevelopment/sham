2e42a0d9683b13606d25125dfa71b06555d1f684
Daniel Karp & Dave Marshall <dave@atst.io>
2015-10-29 15:32:11 +0000

    Fix indent in appropriate place

--- src/Sham/TestDouble/CouldNotVerifyCallWasMadeException.php
+++ src/Sham/TestDouble/CouldNotVerifyCallWasMadeException.php
@@ -22,12 +22,24 @@ class CouldNotVerifyCallWasMadeException extends \Exception
         $methodName = current($this->callsThatWereMade)[0];
         
         $message = "Couldn't verify this call was made\n\n";
-        $message.= CallFormatter::format([$methodName, $this->argsThatCouldNotBeMatched]);
+        $message.= $this->indentCalls([[$methodName, $this->argsThatCouldNotBeMatched]]);
         $message.= "\nThis method was called with:\n\n";
-        foreach ($this->callsThatWereMade as $call) {
-            $message.= CallFormatter::format($call)."\n";
-        }
+        $message.= $this->indentCalls($this->callsThatWereMade);
 
         return $message;
+    }
+
+    private function indentCalls(array $calls)
+    {
+        return implode("\n", array_map(function ($call) {
+            return $this->indent(CallFormatter::format($call));
+        }, $calls));
+    }
+
+    private function indent($string)
+    {
+        return implode("\n", array_map(function ($line) {
+            return $line != "" ? "    ".$line : $line;
+        }, explode("\n", $string)));
     }
 }
