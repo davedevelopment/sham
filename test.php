2d121be154a40640d4937b599cd1cf394c52e154
Daniel Karp & Dave Marshall <dave@atst.io>
2016-03-10 15:39:20 +0000

    Added support for type hints in code generation

--- test.php
+++ test.php
@@ -1,74 +1,101 @@
 <?php
 
-require "vendor/autoload.php";
-require "src/Sham/TestDouble/Stub.php";
-
-use Sham\TestDouble\StubTrait;
-use Sham\TestDouble\Api\Stub;
-
-class User {}
-class Thing 
-{
-    public function foo(int $int, User $user) #: void
-    {
-        return "baz";
-    }
+namespace Foo {
+    class User {}
 }
 
-function stub($class)
-{
-    $rfc = new \ReflectionClass($class);
-    $methods = "";
+namespace {
 
-    foreach ($rfc->getMethods() as $method) {
-
-        $paramArr = [];
-        $params = $method->getParameters();
-        foreach( $params as $param) {
-            $paramString = '';
-            if( $param->hasType()){
-                $paramString .= $param->getType() . " ";
-            }
-            $paramString .= '$' . $param->getName();
-            $paramArr[] = $paramString;
-
-        }
-        $paramsString = implode( ', ', $paramArr );
-
-            var_dump( $paramsString );
-        $methods.= <<<EOS
-        public function {$method->getName()}($paramsString)
-        {
-            return call_user_func_array([\$this, '__call'], ['{$method->getName()}', func_get_args()]);
-        }
-EOS;
-
-    }
-
-    $stubClass = <<<EOS
+    require "vendor/autoload.php";
+    require "src/Sham/TestDouble/Stub.php";
 
     use Sham\TestDouble\StubTrait;
     use Sham\TestDouble\Api\Stub;
 
-    return new class() extends $class implements Stub
+    class Thing 
     {
-        use StubTrait;
+        public function foo(int $int, Foo\User $user) : Foo\User
+        {
+            return new User;
+        }
 
-        $methods
-    };
+        public function fooString(int $int, Foo\User $user) : string
+        {
+            return "bas";
+        }
+
+        public function fooStdclass(int $int, Foo\User $user) : stdclass
+        {
+            return "bas";
+        }
+    }
+
+    function stub($class)
+    {
+        $rfc = new \ReflectionClass($class);
+        $methods = "";
+
+        foreach ($rfc->getMethods() as $method) {
+
+            $paramArr = [];
+            $params = $method->getParameters();
+            foreach( $params as $param) {
+                $paramString = '';
+                if( $param->hasType()){
+                    $paramString .= $param->getType() . " ";
+                }
+                $paramString .= '$' . $param->getName();
+                $paramArr[] = $paramString;
+
+            }
+            $paramsString = implode( ', ', $paramArr );
+
+            $returnType = "";
+
+            if ($method->hasReturnType()) {
+                $asString = (string) $method->getReturnType();
+                if (!$method->getReturnType()->isBuiltin()) {
+                    $asString = "\\".$asString;
+                }
+                $returnType = ": $asString";
+            }
+
+            $methods.= <<<EOS
+            public function {$method->getName()}($paramsString)$returnType
+            {
+                return call_user_func_array([\$this, '__call'], ['{$method->getName()}', func_get_args()]);
+            }
 EOS;
 
-    return eval($stubClass);
-}
+        }
 
-$stub = stub(Thing::class);
+        $stubClass = <<<EOS
 
-$stub->stub("foo")->toReturn("bar");
- 
-if (!$stub instanceof Thing) {
-    throw new \Exception("STUB WOULD NOT SATISFY TYPE HINT");
-}
+        use Sham\TestDouble\StubTrait;
+        use Sham\TestDouble\Api\Stub;
 
-if ($stub->foo( 123, new User) !== "bar") {
-    throw new \Exception("STUB DID NOT WORK");
+        return new class() extends $class implements Stub
+        {
+            use StubTrait;
+
+            $methods
+        };
+EOS;
+
+        return eval($stubClass);
+    }
+
+    $stub = stub(Thing::class);
+
+    $stub->stub("foo")->toReturn(new Foo\User());
+     
+    if (!$stub instanceof Thing) {
+        throw new \Exception("STUB WOULD NOT SATISFY TYPE HINT");
+    }
+
+    if (! $stub->foo( 123, new Foo\User) instanceof Foo\User) {
+        throw new \Exception("STUB DID NOT WORK");
+    }
+
+    var_dump('GOOD');
 }
